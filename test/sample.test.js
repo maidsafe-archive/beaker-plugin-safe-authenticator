603071229ebbbc97035ba2d1676d712707541c22
Shankar <shankar21mail@gmail.com>
2016-11-25 12:17:47 +0530

    test/auth_test_cases: integrate auth test cases
    Integrated auth test cases.
    Integrated unregistered client test cases.

--- test/sample.test.js
+++
@@ -1,8 +0,0 @@
-/* eslint func-names: 0 */
-import should from 'should';
-
-describe('description', () => {
-  it('description', () => {
-    should(1 + 2).be.equal(3);
-  });
-});
--- test/setup.js
+++ test/setup.js
@@ -1,1 +1,23 @@
 import 'babel-polyfill';
+import path from 'path';
+import i18n from 'i18n';
+import ffiLoader from '../src/ffi/ffi_loader';
+
+i18n.configure({
+  locales: ['en'],
+  directory: path.resolve(__dirname, '../', 'locales'),
+  objectNotation: true
+});
+
+i18n.setLocale('en');
+
+const init = async () => {
+  try {
+    // load ffi library
+    await ffiLoader.loadLibrary();
+  } catch (e) {
+    throw (e);
+  }
+};
+
+init();
