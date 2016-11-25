5a994c83b9070b8b4427d02bbb1ff2644fb0474b
Shankar <shankar21mail@gmail.com>
2016-11-25 09:03:46 +0530

    MAID-1746 Test cases for create new account API MAID-1744 Test cases for Login API

--- test/setup.js
+++ test/setup.js
@@ -1,1 +1,11 @@
 import 'babel-polyfill';
+import path from 'path';
+import i18n from 'i18n';
+
+i18n.configure({
+  locales: ['en'],
+  directory: path.resolve(__dirname, '../', 'locales'),
+  objectNotation: true
+});
+
+i18n.setLocale('en');
