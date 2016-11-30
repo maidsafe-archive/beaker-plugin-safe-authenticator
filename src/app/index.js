25f5b5a055e57f10ad2844ba30469af8fdd7ac95
Shankar <shankar21mail@gmail.com>
2016-11-30 17:28:28 +0530

    feat/sass: integrate sass preprocessor
    Integrated SASS preprocessor

--- src/app/index.js
+++ src/app/index.js
@@ -3,6 +3,8 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import App from './app';
+import '../../node_modules/opensans-npm-webfont/style.scss';
+import './sass/main.scss';
 
 render(
   <Provider store={store}>
